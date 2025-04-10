b68b19f7aaa352d3ff0f1ea69d2c225d75baf985
Key Yu Wan <keyflcbyuwan@gmail.com>
2025-04-10 17:51:27 -0300

    refactor: update instructions

--- src/components/app/instructions.tsx
+++ src/components/app/instructions.tsx
@@ -2,7 +2,8 @@ export function Instructions() {
   return (
     <div className="text-zinc-400 space-y-2 mt-16 fixed top-[400px]">
       <p>🔎 Use your mousewheel to zoom in and out</p>
-      <p>🖱️ Use the bottom scrollbar to navigate between the timeline</p>
+      <p>⬇️ Use the bottom scrollbar to navigate between the timeline</p>
+      <p>🖱️ Hover over small items to read about them</p>
     </div>
   );
 }
